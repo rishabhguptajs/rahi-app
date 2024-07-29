18544d672284c3f6609f090a9a0054a1bed9fb53
rishabhguptajs <rishabhgupta4523@gmail.com>
2024-07-29 12:44:44 +0530

    chore: initialized the app's frontend and backend

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+const port = process.env.PORT || 8080
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
