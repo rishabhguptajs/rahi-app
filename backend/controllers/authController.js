74e9e1b5cdd2eafa7a5ed6001d8eec72336ddddb
rishabhguptajs <rishabhgupta4523@gmail.com>
2024-08-06 19:55:21 +0530

    chore: Add login functionality and controller for user login

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,6 @@
 import e from "express";
 import User from "../models/userModel.js";
+import { hashPassword } from "../helpers/authHelpers.js";
 
 export const registerController = async (req, res) => {
     try {
@@ -17,10 +18,16 @@ export const registerController = async (req, res) => {
             return res.status(400).json({ message: 'Password is required', success: false });
         }
 
+        if (password.length < 6) {
+            return res.status(400).json({ message: 'Password must be at least 6 characters', success: false });
+        }
+
+        let hashedPassword = hashPassword(password);
+
         const user = new User({
             name,
             email,
-            password
+            password: hashedPassword
         });
 
         await user.save();
@@ -41,3 +48,44 @@ export const registerController = async (req, res) => {
         });
     }
 };
+
+export const loginController = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email) {
+            return res.status(400).json({ message: 'Email is required', success: false });
+        }
+
+        if (!password) {
+            return res.status(400).json({ message: 'Password is required', success: false });
+        }
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found', success: false });
+        }
+
+        const isMatch = comparePasswords(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Incorrect password', success: false });
+        }
+
+        res.status(200).json({
+            message: 'User logged in successfully',
+            user: {
+                name: user.name,
+                email: user.email
+            },
+            success: true
+        });
+    } catch (error) {
+        console.log('Error in loginController:', error);
+        res.status(500).json({
+            message: error.message,
+            success: false
+        });
+    }
+}
